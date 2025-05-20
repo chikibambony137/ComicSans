97de0d4114f1f9bbc903cd4c2f95131c78892fbf
Anton <137686641+chikibambony137@users.noreply.github.com>
2025-05-20 21:50:41 +0300

    test scss features

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@
 
 <template>
     <header>
-      <label class="head">
+      <label>
           ComicSans
       </label>
     </header>
@@ -12,96 +12,81 @@
     <body>
       <div class="universes">
 
-          <button class="universe" id="dc">
-            <label class="label-bttn">DC</label>
+          <button id="dc">
+            <label>DC</label>
           </button>
           
-          <button class="universe" id="marvel">
-            <label class="label-bttn">Marvel</label>
+          <button id="marvel">
+            <label>Marvel</label>
           </button>
 
-          <button class="universe" id="bubble">
-            <label class="label-bttn">Bubble</label>
+          <button id="bubble">
+            <label>Bubble</label>
           </button>
         
       </div>
     </body>
 </template>
 
-<style scoped>
-    header {
-      width: 100%;
-      height: 100px;
-      display: flex;
-      justify-content: center; /* Центрирует по горизонтали */
-      align-items: center; /* Центрирует по вертикали */
-    }
-
-    .head {
-      position: relative;
-      width: 800px;
-      height: 200px;
-      display: flex;
-      justify-content: center; /* Центрирует содержимое внутри head */
-      align-items: center; /* Центрирует содержимое внутри head */
-
-      color: rgb(255, 255, 255);
-      font-family: 'Sedgwick Ave Display', cursive;
-      font-size: 72px;
-      font-weight: 400;
-      line-height: 160px;
-      letter-spacing: 0%;
-      text-align: center;
-    }
+<style lang="scss" scoped>
 
     .universes {
       width: 100%;
+
       position: relative;
       top: 100px;
+
       display: flex;
       justify-content: center;
       align-items: center;
-    }
 
-    .universe {
-      width: 20%;
-      height: 70px;
-      transform: skew(-20deg);
-      position: relative;
-      margin: 1%;
+      button {
+        width: 20%;
+        height: 70px;
+        transform: skew(-20deg);
+
+        position: relative;
+        margin: 1%;
+
+        label {
+          color: white;
+          transform: none;
+          line-height: 59px;
+          letter-spacing: 0%;
+          text-align: center;
+
+          font: {
+            family: 'Montserrat';
+            size: 36px;
+            weight: 400;
+          }
+        }
+      }
     }
 
     #dc {
-      background: rgba(3, 116, 157, 0.2)
-    }
-    #dc:hover {
-      background: rgba(3, 116, 157, 0.75)
+      background: rgba(3, 116, 157, 0.2);
 
+      &:hover {
+        background: rgba(0, 187, 255, 0.75);
+      }
     }
 
     #marvel {
-      background: rgba(150, 1, 1, 0.2)
-    }
-    #marvel:hover {
-      background: rgba(150, 1, 1, 0.75)
+      background: rgba(150, 1, 1, 0.2);
+
+      &:hover {
+        background: rgba(253, 0, 0, 0.75);
+      }
     }
 
     #bubble {
-      background: rgba(39, 54, 170, 0.2)
-    }
-    #bubble:hover {
-      background: rgba(39, 54, 170, 0.75)
+      background: rgba(39, 54, 170, 0.2);
+
+      &:hover {
+        background: rgba(50, 74, 255, 0.75);
+      }
     }
 
-    .label-bttn {
-      color: rgb(255, 255, 255);
-      font-family: Montserrat;
-      transform: none;
-      font-size: 36px;
-      font-weight: 400;
-      line-height: 59px;
-      letter-spacing: 0%;
-      text-align: center;
-    }
 </style>
 
