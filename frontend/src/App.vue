c7e8b74c9af4dd94f4f4f38c651800380b805892
Anton <137686641+chikibambony137@users.noreply.github.com>
2025-05-27 22:59:46 +0300

    add active style for buttons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -23,19 +23,21 @@
 </template>
 
 <style lang="scss" scoped>
-  $dc-color-button-default: rgba(3, 116, 157, 0.2);
-  $dc-color-button-hover: rgba(0, 187, 255, 0.75);
+$dc-color-button-default: rgba(3, 116, 157, 0.2);
+$dc-color-button-hover: rgba(0, 187, 255, 0.75);
+$dc-color-button-active: rgba(0, 127, 174, 0.75);
 
-  $marvel-color-button-default: rgba(150, 1, 1, 0.2);
-  $marvel-color-button-hover: rgba(253, 0, 0, 0.75);
+$marvel-color-button-default: rgba(150, 1, 1, 0.2);
+$marvel-color-button-hover: rgba(253, 0, 0, 0.75);
+$marvel-color-button-active: rgba(206, 0, 0, 0.75);
 
-  $bubble-color-button-default: rgba(39, 54, 170, 0.2);
-  $bubble-color-button-hover: rgba(50, 74, 255, 0.75);
+$bubble-color-button-default: rgba(39, 54, 170, 0.2);
+$bubble-color-button-hover: rgba(50, 74, 255, 0.75);
+$bubble-color-button-active: rgba(28, 46, 184, 0.75);
 
 .universes {
   width: 100%;
-
-    position: relative;
+  position: relative;
     top: 100px;
   
     display: flex;
@@ -72,6 +74,14 @@
     &:hover {
       background: $dc-color-button-hover;
     }
+  
+    &:active {
+      background: $dc-color-button-active;
+  
+      label {
+        color: rgb(153, 153, 153);
+      }
+    }
   }
   
   #marvel {
@@ -79,6 +89,14 @@
   
     &:hover {
       background: $marvel-color-button-hover;
+    }
+  
+    &:active {
+      background: $marvel-color-button-active;
+  
+      label {
+        color: rgb(153, 153, 153);
+      }
     }
   }
   
@@ -88,5 +106,13 @@
     &:hover {
       background: $bubble-color-button-hover;
     }
-  }
+  
+    &:active {
+      background: $bubble-color-button-active;
+  
+      label {
+        color: rgb(153, 153, 153);
+      }
+    }
+}
 </style>
